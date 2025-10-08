1b234b2f9c9d33d4004f4f1e5f21e2855931456e
henriqueECM <henriqueworks68@gmail.com>
2025-10-08 16:06:17 -0300

    feat: add model aula

---
+++ src/main/java/com/example/gestaoEscolar/model/Aula.java
@@ -0,0 +1,55 @@
+package com.example.gestaoEscolar.model;
+
+import java.time.LocalDateTime;
+
+public class Aula {
+    private int id, turmaId;
+    private LocalDateTime dataHora;
+    private String assunto;
+
+    public Aula(int turmaId, LocalDateTime dataHora, String assunto) {
+        this.turmaId = turmaId;
+        this.dataHora = dataHora;
+        this.assunto = assunto;
+    }
+
+    public Aula(int id, int turmaId, LocalDateTime dataHora, String assunto) {
+        this.id = id;
+        this.turmaId = turmaId;
+        this.dataHora = dataHora;
+        this.assunto = assunto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTurmaId() {
+        return turmaId;
+    }
+
+    public void setTurmaId(int turmaId) {
+        this.turmaId = turmaId;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getAssunto() {
+        return assunto;
+    }
+
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+
+}
--- src/main/java/com/example/gestaoEscolar/repository/TurmaAlunoDAO.java
+++ src/main/java/com/example/gestaoEscolar/repository/TurmaAlunoDAO.java
@@ -6,7 +6,6 @@ import com.example.gestaoEscolar.model.TurmaAluno;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TurmaAlunoDAO {
     public TurmaAluno create (TurmaAluno turmaAluno) throws SQLException {
